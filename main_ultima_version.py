70f37805f146dc5bcc627d22ea3f7f77c442b5cd
Sammy Pineda <sampgdev@gmail.com>
2025-09-18 15:07:54 -0500

    feat: agregar selección de carpeta y mejorar manejo de errores en la descarga y combinación de videos

--- main_ultima_version.py
+++ main_ultima_version.py
@@ -33,7 +33,6 @@ def run_vimeo_dl(url, progress_bar, result_text, storage_path):
 
     command = f'{vimeo_dl_path} -i "{url}"'
     result_text.value = "Descargando..."
-    result_text.update()
     try:
         progress_bar.visible = True
         progress_bar.value = 0
@@ -110,12 +109,21 @@ def main(page: flet.Page):
 
     storage_path = ""
 
+    def pick_folder(e: FilePickerResultEvent):
+        nonlocal storage_path
+        if e.path:
+            storage_path = e.path
+            storage_field.value = storage_path
+            storage_field.update()
+        else:
+            storage_path = ""
 
-    url_field = TextField(hint_text="Ingresa la URL de Vimeo",disabled=True,
+    url_field = TextField(hint_text="Ingresa la URL de Vimeo",
                           on_change=lambda e: validate_url(e))
-    output_field = TextField(hint_text="Salida (ej: video.mp4)", disabled=True)
+    output_field = TextField(hint_text="Salida (ej: video.mp4)")
     storage_field = TextField(
         hint_text="Ruta de almacenamiento (ej: C:\\Videos)", disabled=True)
+    storage_picker = FilePicker(on_result=pick_folder)
 
     progress_bar = ProgressBar(width=300, height=3, visible=False)
     result_text = Text()
@@ -126,20 +134,6 @@ def main(page: flet.Page):
     combine_button.disabled = True
     storage_button = ElevatedButton(
         "Seleccionar carpeta", on_click=lambda _: storage_picker.get_directory_path())
-    
-    def pick_folder(e: FilePickerResultEvent):
-        nonlocal storage_path
-        if e.path:
-            storage_path = e.path
-            storage_field.value = storage_path
-            url_field.disabled = False
-            output_field.disabled = False
-            url_field.update()
-            output_field.update()
-            storage_field.update()
-        else:
-            storage_path = ""
-    storage_picker = FilePicker(on_result=pick_folder)
 
     def validate_url(e):
         url = url_field.value
@@ -163,7 +157,7 @@ def main(page: flet.Page):
             return
 
         output_file = output_field.value.strip() + ".mp4"
-        if (os.path.exists(output_file) or os.path.exists(clip_id + "-video.mp4")):
+        if output_file and (os.path.exists(output_file) or os.path.exists(clip_id + "-video.mp4")):
             result_text.value = f"Advertencia: El video ya existe"
             result_text.update()
             download_button.disabled = True
@@ -240,12 +234,13 @@ def main(page: flet.Page):
     def restart_app(e):
         url_field.value = ""
         output_field.value = ""
-        
+        storage_field.value = ""
         result_text.value = ""
         progress_bar.value = 0
         progress_bar.visible = False
         url_field.update()
         output_field.update()
+        storage_field.update()
         result_text.update()
         progress_bar.update()
         finish_dialog.open = False
@@ -269,7 +264,7 @@ def main(page: flet.Page):
     page.add(
         Column(
             [
-                Text("VimeoDL GUI", style="headlineSmall"),
+                Text("Vimeo Downloader", style="headlineSmall"),
                 url_field,
                 output_field,
                 storage_field,
